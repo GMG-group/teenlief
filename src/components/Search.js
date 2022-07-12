1a4d21829dc3d1bc86b0283bde10858e062cf059
PRESSO <caff1nepill@gmail.com>
2022-07-12 23:11:41 +0900

    add filter tag

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {TextInput, View, StyleSheet, Dimensions} from "react-native";
+import {TextInput, View, StyleSheet, Dimensions, Text, FlatList, ScrollView} from "react-native";
 import Icon from "react-native-vector-icons/dist/Feather";
 
 const vw = Dimensions.get('window').width;
@@ -7,30 +7,55 @@ const vh = Dimensions.get('window').height;
 
 const Search = () => {
 	const [search, setSearch] = useState(null);
+	const filterTag = ["숙식", "숙식", "숙식", "숙식", "숙식", "숙식"]
 
 	return (
-		<View style={[ styles.search ]}>
-			<View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-				<Icon name="menu" size={25} color={'#000'} style={{ marginRight: 10 }} />
-				<TextInput
-					style={styles.input}
-					onChangeText={setSearch}
-					value={search}
-					placeholder="여기서 검색"
-				/>
+		<View style={styles.container}>
+			<View style={[ styles.search ]}>
+				<View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+					<Icon name="menu" size={25} color={'#000'} style={{ marginRight: 10 }} />
+					<TextInput
+						style={styles.input}
+						onChangeText={setSearch}
+						value={search}
+						placeholder="여기서 검색"
+					/>
+				</View>
+				<View style={[ styles.profileImage ]}></View>
 			</View>
-			<View style={[ styles.profileImage ]}></View>
+
+			<ScrollView
+				style={styles.filter}
+				horizontal={true}
+				scrollEnabled={true}
+			>
+				{
+					filterTag.map((item, index) => {
+						return (
+							<View style={styles.filterItem} key={index}>
+								<Text style={{color: 'black'}}>{ item }</Text>
+							</View>
+						)
+					})
+				}
+			</ScrollView>
+
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
+	container: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		position: 'absolute',
+		alignSelf: 'center',
+		alignItems: 'center',
+	},
 	search: {
 		display: 'flex',
 		justifyContent: 'space-between',
 		flexDirection: 'row',
-		position: 'absolute',
-		alignSelf: 'center',
 		width: 0.9 * vw,
 		height: 50,
 		borderRadius: 0.075 * vw,
@@ -56,6 +81,23 @@ const styles = StyleSheet.create({
 	},
 	input: {
 		width: 0.6 * vw
+	},
+	filter: {
+		display: "flex",
+		flexDirection: "row",
+		width: 0.9 * vw,
+		marginTop: 10
+	},
+	filterItem: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		width: 90,
+		height: 30,
+		backgroundColor: "white",
+		borderWidth: 1,
+		borderRadius: 50,
+		marginRight: 10
 	}
 });
 
