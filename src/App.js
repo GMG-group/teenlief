c0a5f133b6a1d5a778b4404dde098c47f65f5f2b
euichan2 <euichan1108@gmail.com>
2022-07-25 04:07:52 +0900

    add loading text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {RecoilRoot} from "recoil";
 import Main from "@screens/Main"
+import {Text} from "react-native";
 
 const App = () => {
 	return (
 		<RecoilRoot>
-			<React.Suspense>
+			<React.Suspense fallback={<Text>Loading...</Text>}>
 				<Main />
 			</React.Suspense>
 		</RecoilRoot>
