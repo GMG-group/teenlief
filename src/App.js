be851d0f4fdd750baced77c43b2924fe4d182952
PRESSO <caff1nepill@gmail.com>
2022-07-29 10:58:33 +0900

    add bottom sheet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {RecoilRoot} from "recoil";
 import Main from "@screens/Main"
+import {BottomSheetModalProvider} from "@gorhom/bottom-sheet";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 const App = () => {
 	return (
 		<RecoilRoot>
-			<Main />
+			<GestureHandlerRootView style={{flex: 1}}>
+				<BottomSheetModalProvider>
+					<Main />
+				</BottomSheetModalProvider>
+			</GestureHandlerRootView>
 		</RecoilRoot>
 	)
 }
--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import NaverMapView from "react-native-nmap";
+import React, {useMemo, useRef} from 'react';
+import { Text, View } from "react-native";
+import NaverMapView, { Marker } from "react-native-nmap";
 import Search from "@components/Search";
-import { View } from "react-native";
+import {BottomSheetModal} from "@gorhom/bottom-sheet";
 
 const Map = () => {
+	// ref
+	const bottomSheetModalRef = useRef(null);
+
+	// variables
+	const snapPoints = useMemo(() => ['25%', '50%'], []);
+
 	return (
-		<View>
+		<>
+			<BottomSheetModal
+				ref={bottomSheetModalRef}
+				index={1}
+				snapPoints={snapPoints}
+			>
+				<Text>Awesome 🎉</Text>
+			</BottomSheetModal>
+
 			<NaverMapView
 				style={{width: '100%', height: '100%'}}
-				showsMyLocationButton={true}
+				showsMyLocationButton={false}
 			>
+				<Marker coordinate={{latitude: 37.5828, longitude: 127.0107}} onClick={() => {
+					console.log("click");
+					bottomSheetModalRef.current?.present();
+				}} />
 			</NaverMapView>
 
 			<Search />
-		</View>
+		</>
 	);
 };
 
