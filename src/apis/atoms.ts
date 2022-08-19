6d13633296b57144f654aaaf2d9728caac7352a6
euichan2 <euichan1108@gmail.com>
2022-08-19 22:44:19 +0900

    storage token

--- src/apis/atoms.ts
+++ src/apis/atoms.ts
@@ -1,4 +1,5 @@
 import { atom } from "recoil";
+import ReactNativeRecoilPersist from "react-native-recoil-persist";
 
 export interface Token {
     accessToken: string;
@@ -14,7 +15,8 @@ export const tokenState = atom<Token> ({
     default: {
         accessToken: "",
         refreshToken: ""
-    }
+    },
+    effects_UNSTABLE: [ReactNativeRecoilPersist.persistAtom],
 });
 
 export const userInfoState = atom<UserInfo> ({
