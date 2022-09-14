73526d26f7da0b6f18737c200a1bf99b68f8b50a
PRESSO <caff1nepill@gmail.com>
2022-09-15 00:21:57 +0900

    add chat with connect button

--- src/apis/apiServices.ts
+++ src/apis/apiServices.ts
@@ -133,3 +133,15 @@ export const checkUserMarkerExists = (header: any, id: any):Promise<AxiosRespons
         }
     )
 }
+
+export const postChatRoom = (header: any, body: any):Promise<AxiosResponse> => {
+    return axios.post(
+        `${URL}/api/chat/`,
+        body, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                ...header
+            }
+        }
+    )
+}
