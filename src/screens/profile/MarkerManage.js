f5005dbf3182c00893cc78a91ef01278ef2fadae
euichan2 <euichan1108@gmail.com>
2022-10-23 17:47:53 +0900

    delete

--- src/screens/profile/MarkerManage.js
+++ src/screens/profile/MarkerManage.js
@@ -2,17 +2,22 @@ import React, {useEffect} from "react";
 import {ScrollView, StyleSheet, Text, View} from "react-native";
 import MarkerCard from "@components/MarkerCard";
 import Header from "@components/Header";
-import {getMyMarker} from "@apis/apiServices";
+import {deleteMarker, getMyMarker} from "@apis/apiServices";
 import useApi from "@apis/useApi";
 
 const MarkerManage = ({navigation}) => {
     const [myMarkerLoading, myMarkerResolved, myMarkerApi] = useApi(getMyMarker, true);
+    const [deleteLoading, deleteResolved, deleteApi, setDeleteLoading] = useApi(deleteMarker, true);
+
+    useEffect(() => {
+        if(!deleteLoading) {
+            myMarkerApi()
+            setDeleteLoading(true);
+        }
+    },[deleteLoading])
 
     useEffect(() => {
        myMarkerApi()
-           .then((response) => {
-               console.log("finish", response);
-           })
     },[])
 
     if(myMarkerLoading) {
@@ -28,7 +33,7 @@ const MarkerManage = ({navigation}) => {
 
                 {
                     myMarkerResolved.map((marker, idx) => (
-                        <MarkerCard style={styles.markerCard} key={`MarkerCard-${idx}`} marker={marker}/>
+                        <MarkerCard style={styles.markerCard} key={`MarkerCard-${idx}`} marker={marker} deleteApi={deleteApi}/>
                     ))
                 }
             </ScrollView>
