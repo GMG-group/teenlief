a18ff5e5956b1024362c389fbc55d651a639e0f1
PRESSO <caff1nepill@gmail.com>
2022-11-08 04:12:39 +0900

    add gender in certification

--- src/screens/Certification.js
+++ src/screens/Certification.js
@@ -1,24 +1,35 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableHighlight, View} from "react-native";
+import {
+    Animated,
+    KeyboardAvoidingView,
+    Platform,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableHighlight,
+    View
+} from "react-native";
 import {useRecoilState} from "recoil";
 import {ACTION, SCREEN, actionState, userState} from "@apis/atoms";
 import useApi from "@apis/useApi";
 import {getUser, postCertificate} from "@apis/apiServices";
 import {vh, vw} from "react-native-css-vh-vw";
-import VerifyCertification from "@screens/VerifyCertification";
+import SwitchSelector from "react-native-switch-selector";
+
+const AnimationSwitchSelector = Animated.createAnimatedComponent(SwitchSelector);
+const options = [
+    { label: '남성', value: 'M' },
+    { label: '여성', value: 'F' },
+];
 
 const Certification = ({ navigation }) => {
-    const [action, setAction] = useRecoilState(actionState);
-    const [user, setUser] = useRecoilState(userState);
-
     const [phone, setPhone] = useState("");
-
-    const [getUserLoading, getUserResult, getUserApi] = useApi(getUser, true);
+    const [gender, setGender] = useState("M");
     const [postCertificateLoading, postCertificateResult, postCertificateApi] = useApi(postCertificate, true);
-
 
     const requestCertificate = () => {
         const formData = new FormData();
+        formData.append("gender", gender);
         formData.append("phone", phone);
         postCertificateApi(formData)
             .then((res) => {
@@ -32,16 +43,25 @@ const Certification = ({ navigation }) => {
         <KeyboardAvoidingView style={styles.container}>
             <View style={styles.textContainer}>
                 <View style={{ marginBottom: vh(1) }}>
-                    <Text style={styles.mainText}>이름과 휴대폰 번호를 입력해주세요.</Text>
+                    <Text style={styles.mainText}>이름과 성별, 휴대폰 번호를 입력해주세요.</Text>
                     <Text style={styles.subText}>본인 인증을 위해 필요합니다.</Text>
                 </View>
                 <TextInput style={{ borderBottomWidth: 1, marginBottom: vh(2) }} placeholder={"이름"} />
                 <TextInput style={{ borderBottomWidth: 1 }} value={phone} onChangeText={setPhone} placeholder={"전화번호"} />
-
+                <AnimationSwitchSelector
+                    style={{ width: '100%', marginTop: vh(5) }}
+                    options={options}
+                    initial={0}
+                    hasPadding={true}
+                    textColor='black'
+                    buttonColor={'#AE46FF'}
+                    height={40}
+                    onPress={(value) => setGender(value)}
+                />
             </View>
 
             <TouchableHighlight
-                style={styles.submitButton}
+                style={styles.submitButton}이강혁
                 onPress={requestCertificate}
             >
                 <View>
@@ -64,7 +84,7 @@ const styles = StyleSheet.create({
         marginTop: vh(16)
     },
     mainText: {
-        fontSize: 20,
+        fontSize: 18,
         fontWeight: 'bold',
         color: "#252525"
     },
